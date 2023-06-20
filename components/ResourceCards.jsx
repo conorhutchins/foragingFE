b22c58fcd7674a5094e37c54e532d7c1d9dd4d2e
conorhutchins <conorhutchins@btinternet.com>
2023-06-20 17:27:24 +0100

    refactored the distance

--- components/ResourceCards.jsx
+++ components/ResourceCards.jsx
@@ -8,12 +8,30 @@ export default function ResourceCards({ cardPress, resources, navigation, locati
   };
 
   const calculateDistance = (lat1, lon1, lat2, lon2) => {
-    const distance = getDistance(
-      { latitude: lat1, longitude: lon1 },
-      { latitude: lat2, longitude: lon2 }
-    )
-    return (distance / 1000).toFixed(2);
+    const radius = 6371;
+    
+    const lat1Rad = toRadians(lat1);
+    const lon1Rad = toRadians(lon1);
+    const lat2Rad = toRadians(lat2);
+    const lon2Rad = toRadians(lon2);
+  
+    // Difference between the latitudes and longitudes
+    const deltaLat = lat2Rad - lat1Rad;
+    const deltaLon = lon2Rad - lon1Rad;
+  
+    // Haversine formula
+    const a = Math.sin(deltaLat/2) ** 2 + Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.sin(deltaLon/2) ** 2;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  
+    // Calculate the distance
+    const distance = radius * c;
+    return distance;
   }
+  
+  function toRadians(degrees) {
+    return degrees * Math.PI / 180;
+  }
+  
 
   const renderItem = ({ item }) => {
     const coords = item.location.split(',');
