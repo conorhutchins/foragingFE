e8d8b66c3342b2c94e640f8adaf1a36b92d682f5
conorhutchins <conorhutchins@btinternet.com>
2023-06-21 14:00:10 +0100

    adds styling to welcome page

--- pages/Welcome.jsx
+++ pages/Welcome.jsx
@@ -1,7 +1,8 @@
 import { useState, useContext, useEffect } from "react";
-import { View, Text, TextInput, Button, StyleSheet, Alert, KeyboardAvoidingView } from "react-native";
+import { View, Text, TextInput, Button, StyleSheet, Alert, KeyboardAvoidingView, TouchableOpacity, Image } from "react-native";
 import { UserContext } from "../contexts/UserContext";
 import { Formik } from "formik";
+
 
 export default function Welcome({ navigation }) {
   
@@ -22,13 +23,18 @@ export default function Welcome({ navigation }) {
     behavior={Platform.OS === 'ios' ? 'padding' : undefined}
     keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0} // Adjust this offset if needed
   >
-    <View style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.title}>
+          <Text style={styles.titleText}>Nourish & Gather</Text>
+        </View>
+        <Image source={require('../assets/logo.png')} style={{ width: 250, height: 250, alignSelf: 'center', marginTop: 100, marginBottom: 50}} />
+      <View style={styles.warning}>
       <Text style={styles.label}>Some things to note from the Devs</Text>
-      <Text style={styles.subLabel}>- Do not use this app to find food.</Text>
-      <Text style={styles.subLabel}>- Do not travel to the places listed.</Text>
-      <Text style={styles.subLabel}>- Do not eat any food you find when you don't travel there.</Text>
-  
-      <Text style={styles.label}>Please enter your username:</Text>
+      <Text style={styles.subLabel}>- Do not use this app to find food -</Text>
+      <Text style={styles.subLabel}>- Do not travel to the places listed -</Text>
+      <Text style={styles.subLabel}>- Do not eat any food you find when you don't travel there -</Text>
+      </View>
+      <Text style={styles.usernameLabel}>Choose your username</Text>
       <Formik initialValues={{ username: "" }} onSubmit={handleSubmit}>
         {({ handleChange, handleBlur, handleSubmit, values }) => (
           <>
@@ -40,7 +46,9 @@ export default function Welcome({ navigation }) {
               value={values.username}
             />
             <View style={styles.button}>
-              <Button color="white" onPress={handleSubmit} title="Submit" />
+            <TouchableOpacity style={styles.button}>
+           <Text style={styles.buttonText}>Submit</Text>
+            </TouchableOpacity>
             </View>
           </>
         )}
@@ -51,28 +59,77 @@ export default function Welcome({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+  title: {
+    position: 'absolute',
+    top: 100,
+    alignSelf: 'center',
+  },
+  titleText: {
+    textAlign: 'center',
+    fontSize: 45,
+    fontWeight: "bold",
+    color: '#fff68f',
+    
+
+  },
   container: {
     flex: 1,
     justifyContent: "center",
     padding: 16,
+    backgroundColor:"#35bb42",
   },
   label: {
     marginBottom: 8,
     fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 15,
+    color: '#492c03',
   },
   subLabel: {
     marginBottom: 8,
+    textAlign: 'center',
+    fontSize: 13,
+    color: '#492c03',
+  },
+  usernameLabel: {
+    marginBottom: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 22,
+    color: '#492c03',
   },
   input: {
     height: 50,
     borderColor: "gray",
     borderWidth: 1,
-    marginBottom: 16,
+    marginBottom: 100,
     paddingHorizontal: 8,
     paddingVertical: 10,
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    textAlign: 'center',
+    marginTop: 10,
+    width: "67%",
+    alignSelf: 'center', // Align the button horizontally in the center
+    justifyContent: 'center', // Align the button vertically in the cente
   },
+  warning: {
+    marginBottom: 20,
+  },
+
   button: {
-    backgroundColor: "#007BFF",
-    borderRadius: 5,
-  }
+    backgroundColor: '#fff68f',
+    padding: 8,
+    borderRadius: 25,
+    width: "67%",
+    alignSelf: 'center', // Align the button horizontally in the center
+    justifyContent: 'center', // Align the button vertically in the center
+  },
+  buttonText: {
+    color: '#492c03',
+    fontSize: 20, // Change this value to adjust the font size
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
+
