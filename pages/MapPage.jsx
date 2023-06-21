74abbe32d775df92846b3d4c30c62d8e672fb0d2
conorhutchins <conorhutchins@btinternet.com>
2023-06-21 16:58:35 +0100

    sorts keyboard avoiding view colour error

--- pages/MapPage.jsx
+++ pages/MapPage.jsx
@@ -86,7 +86,7 @@ export default function MapPage({ navigation }) {
   }
   return (
     <KeyboardAvoidingView
-      style={{ flex: 1 }}
+      style={{ flex: 1, backgroundColor: "#36d346"}}
       behavior={Platform.OS === "ios" ? "padding" : undefined}
       keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
     >
